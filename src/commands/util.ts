843ab3d341f06927c1ece8b92d59895db3aae026
Gavenda <gavenda@disroot.org>
2023-04-24 12:03:04 +0800

    Remove getValue typings, we don't really need all that

--- src/commands/util.ts
+++ src/commands/util.ts
@@ -72,6 +72,18 @@ export const titleCase = (str: string): string => {
 };
 
 export const htmlToMarkdown = (str: string): string => {
+  str = decodeURI(str);
+  const linkMatch = str.matchAll(/<a href="(.*)">(.*)<\/a>/gm);
+
+  for (const match of linkMatch) {
+    console.log(`Match 0:`);
+    console.log(match[0]);
+    console.log(`Match 1:`);
+    console.log(match[1]);
+    console.log(`Match 2:`);
+    console.log(match[2]);
+  }
+
   return str
     .replaceAll('<b>', '**')
     .replaceAll('</b>', '**')
@@ -142,39 +154,13 @@ export const mediaFormatDisplay = (format: MediaFormat) => {
   }
 };
 
-type GetIndexedField<T, K> = K extends keyof T ? T[K] : K extends `${number}` ? ('0' extends keyof T ? undefined : number extends keyof T ? T[number] : undefined) : undefined;
-
-type FieldWithPossiblyUndefined<T, Key> = GetFieldType<Exclude<T, undefined>, Key> | Extract<T, undefined>;
-
-type IndexedFieldWithPossiblyUndefined<T, Key> = GetIndexedField<Exclude<T, undefined>, Key> | Extract<T, undefined>;
-
-export type GetFieldType<T, P> = P extends `${infer Left}.${infer Right}`
-  ? Left extends keyof T
-    ? FieldWithPossiblyUndefined<T[Left], Right>
-    : Left extends `${infer FieldKey}[${infer IndexKey}]`
-    ? FieldKey extends keyof T
-      ? FieldWithPossiblyUndefined<IndexedFieldWithPossiblyUndefined<T[FieldKey], IndexKey>, Right>
-      : undefined
-    : undefined
-  : P extends keyof T
-  ? T[P]
-  : P extends `${infer FieldKey}[${infer IndexKey}]`
-  ? FieldKey extends keyof T
-    ? IndexedFieldWithPossiblyUndefined<T[FieldKey], IndexKey>
-    : undefined
-  : undefined;
-
-export function getValue<TData, TPath extends string, TDefault = GetFieldType<TData, TPath>>(
-  data: TData,
-  path: TPath,
-  defaultValue?: TDefault
-): GetFieldType<TData, TPath> | TDefault {
+export function getValue(data: any, path: string, defaultValue?: any): any {
   const value = path
     .split(/[.[\]]/)
     .filter(Boolean)
-    .reduce<GetFieldType<TData, TPath>>((value, key) => (value as any)?.[key], data as any);
+    .reduce((value, key) => (value as any)?.[key], data as any);
 
-  return value !== undefined ? value : (defaultValue as TDefault);
+  return value !== undefined ? value : defaultValue;
 }
 
 export const distinctByKey = <T>(array: any[], key: string): T[] => {
