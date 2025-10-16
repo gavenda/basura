209fa8adea3649ca3b2cf038aceb1b260f9bd1d8
Enda <enda@gavenda.dev>
2025-10-16 14:39:20 +0800

    Move mediaToComponents to media.ts

--- src/anilist/media.ts
+++ src/anilist/media.ts
@@ -1,9 +1,20 @@
-import { APIApplicationCommandOptionChoice, LocalizationMap } from 'discord-api-types/v10';
-import { truncate } from '../utils/strings';
+import {
+  APIApplicationCommandOptionChoice,
+  APIComponentInContainer,
+  APIComponentInMessageActionRow,
+  APIMessageTopLevelComponent,
+  APITextDisplayComponent,
+  ButtonStyle,
+  ComponentType,
+  LocalizationMap
+} from 'discord-api-types/v10';
+import { NodeHtmlMarkdown } from 'node-html-markdown-cloudflare';
+import { toStars } from '../utils/anilist';
+import { titleCase, truncate } from '../utils/strings';
 import { aniListRequest } from './anilist';
 import { findMediaByIdQuery } from './gql/find-media-by-id';
 import { findMediaTitleQuery } from './gql/find-media-title';
-import { Media, MediaType, Query } from './gql/types';
+import { Media, MediaRankType, MediaType, Query } from './gql/types';
 
 export const findMediaById = async (options: {
   mediaId: number;
@@ -81,4 +92,198 @@ export const findMediaTitles = async (options: {
     console.error({ message: `Error when finding media titles`, error });
     return [];
   }
+};
+
+export const mediaToComponents = (media: Media): APIMessageTopLevelComponent[] => {
+  const detailedInfo: string[] = [];
+  const actionRowComponents: APIComponentInMessageActionRow[] = [];
+
+  if (media.format) {
+    actionRowComponents.push({
+      custom_id: 'media:format',
+      type: ComponentType.Button,
+      style: ButtonStyle.Secondary,
+      label: media.format,
+      disabled: true
+    });
+  }
+
+  if (media.season && media.seasonYear) {
+    actionRowComponents.push({
+      custom_id: 'media:season',
+      type: ComponentType.Button,
+      style: ButtonStyle.Secondary,
+      label: `${titleCase(media.season)} ${media.seasonYear}`,
+      disabled: true
+    });
+  }
+
+  if (media.genres) {
+    detailedInfo.push(`${media.genres.map((x) => `${x}`).join(` - `)}`);
+  }
+
+  if (media.episodes) {
+    detailedInfo.push(`${media.episodes} Episodes`);
+    actionRowComponents.push({
+      custom_id: 'media:parts',
+      type: ComponentType.Button,
+      style: ButtonStyle.Secondary,
+      label: `${media.episodes} Episodes`,
+      disabled: true
+    });
+  }
+
+  if (media.chapters) {
+    detailedInfo.push(`${media.chapters} Chapters`);
+    actionRowComponents.push({
+      custom_id: 'media:parts',
+      type: ComponentType.Button,
+      style: ButtonStyle.Secondary,
+      label: `${media.chapters} Chapters`,
+      disabled: true
+    });
+  }
+
+  if (media.rankings) {
+    const mediaRankingsAsc = media.rankings.sort((a, b) => a.rank - b.rank);
+    const allTimeRank = mediaRankingsAsc.find((x) => x.type === MediaRankType.RATED && x.allTime);
+    const seasonRank = mediaRankingsAsc.find((x) => x.type === MediaRankType.RATED && !x.allTime && x.season);
+
+    if (allTimeRank) {
+      actionRowComponents.push({
+        custom_id: 'media:rank',
+        type: ComponentType.Button,
+        style: ButtonStyle.Secondary,
+        label: `Rank #${allTimeRank.rank}`,
+        disabled: true
+      });
+    } else if (seasonRank && seasonRank.season) {
+      actionRowComponents.push({
+        custom_id: 'media:rank',
+        type: ComponentType.Button,
+        style: ButtonStyle.Secondary,
+        label: `Rank #${seasonRank.rank} of ${titleCase(seasonRank.season)} ${seasonRank.year}`,
+        disabled: true
+      });
+    }
+  }
+
+  if (media.meanScore && media.meanScore != 0) {
+    // footerInfo.push(`Rating - ${media.meanScore} / 100`);
+    actionRowComponents.push({
+      custom_id: 'media:rating',
+      type: ComponentType.Button,
+      style: ButtonStyle.Secondary,
+      label: `${toStars(media.meanScore)}`,
+      disabled: true
+    });
+  }
+
+  const sourceRegex = /^(\(Source: .*\))$/gm;
+
+  let description = '-# No synopsis available.';
+
+  if (media.description) {
+    description = NodeHtmlMarkdown.translate(media.description);
+  }
+
+  // Apply source regex
+  description = description.replaceAll(sourceRegex, (replace) => `-# ${replace}`);
+
+  const components: APIComponentInContainer[] = [];
+
+  // Banner Image, if applicable
+  if (media.bannerImage) {
+    components.push({
+      type: ComponentType.MediaGallery,
+      items: [
+        {
+          media: {
+            url: media.bannerImage
+          }
+        }
+      ]
+    });
+  }
+
+  const titleComponents: APITextDisplayComponent[] = [];
+
+  titleComponents.push({
+    type: ComponentType.TextDisplay,
+    content: `## ${media.title?.english || media.title?.romaji || media.title?.native}`
+  });
+
+  let altTitles = '';
+
+  if (media.title?.english && media.title.romaji) {
+    altTitles = altTitles + `-# _(Romaji: ${media.title.romaji})_\n`;
+  }
+
+  if (media.title?.native) {
+    altTitles = altTitles + `-# _(Native: ${media.title.native})_\n`;
+  }
+
+  if (media.synonyms) {
+    altTitles = altTitles + `-# _(Synonym: ${media.synonyms[0]})_\n`;
+  }
+
+  if (altTitles.length > 0) {
+    titleComponents.push({
+      type: ComponentType.TextDisplay,
+      content: altTitles
+    });
+  }
+
+  // Title
+  components.push({
+    type: ComponentType.Section,
+    components: titleComponents,
+    accessory: {
+      type: ComponentType.Button,
+      style: ButtonStyle.Link,
+      label: 'AniList',
+      url: `${media.siteUrl}`
+    }
+  });
+
+  components.push({
+    type: ComponentType.TextDisplay,
+    content: `-# ${detailedInfo.join(` — `)}`
+  });
+
+  // Seperator
+  components.push({
+    type: ComponentType.Separator
+  });
+
+  if (actionRowComponents.length > 0) {
+    components.push({
+      type: ComponentType.ActionRow,
+      components: actionRowComponents
+    });
+  }
+
+  // Description
+  components.push({
+    type: ComponentType.Section,
+    components: [
+      {
+        type: ComponentType.TextDisplay,
+        content: description
+      }
+    ],
+    accessory: {
+      type: ComponentType.Thumbnail,
+      media: {
+        url: `${media.coverImage?.extraLarge}`
+      }
+    }
+  });
+
+  return [
+    {
+      type: ComponentType.Container,
+      components
+    }
+  ];
 };
